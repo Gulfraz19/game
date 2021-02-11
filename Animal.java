36fe65c99bc6cc2202df84600c095351cc9256da
Gulfraz19 <78660806+Gulfraz19@users.noreply.github.com>
2021-02-11 20:11:24 +0100

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,16 @@
+package com.game;
+
+abstract class Animal
+{
+
+    abstract void set_data(String name, String gender);
+    abstract void set_health(int health);
+    abstract void set_price(double price);
+    abstract String get_name();
+    abstract String get_gender();
+    abstract double get_price();
+    abstract int get_health();
+    abstract int get_Age();
+    abstract void setAge(int age);
+
+}
